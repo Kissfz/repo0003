d30a50480b21ec804caf7cca5a4183b45b3a7de9
Kissfz <34333596+Kissfz@users.noreply.github.com>
2017-12-08 15:05:12 +0800

    Update Test01.java
    second commit

--- src/com/itheima/test/Test01.java
+++ src/com/itheima/test/Test01.java
@@ -3,6 +3,7 @@ package com.itheima.test;
 public class Test01 {
 
     public static void main(String[] args) {
+        //从GitHub下载
         // TODO Auto-generated method stub
         System.out.println(new Test01().test());;
     }
